a4e50600bd59ae10fa8985a2cee4016ab8ad5175
Michael Desanker <mdesanker@gmail.com>
2022-03-09 05:56:41 -0500

    Fix image size on upload

--- src/components/fileUpload/FileUpload.tsx
+++ src/components/fileUpload/FileUpload.tsx
@@ -17,22 +17,30 @@ const FileUpload = () => {
   };
 
   return (
-    <div className="h-60 w-9/10 max-w-[600px] bg-white rounded-lg shadow-lg p-6 flex flex-col justify-center items-center">
+    <div className="h-[400px] w-9/10 max-w-[600px] bg-white rounded-lg shadow-lg p-6 flex flex-col justify-center items-center">
       <label
         htmlFor="select-image"
-        className="w-full h-full rounded-lg bg-gray-100 p-6 border-dashed border-2 border-gray-300 flex flex-col justify-center items-center cursor-pointer"
+        className={`w-full h-full rounded-lg bg-gray-100 p-6 border-dashed border-2 border-gray-300 flex flex-col justify-center items-center cursor-pointer ${
+          imageURL ? "hidden" : ""
+        }`}
       >
         <BsCardImage className="h-10 w-10 text-gray-400" />
         <p className="text-center text-gray-500">Select an image to process</p>
+        <input
+          type="file"
+          id="select-image"
+          accept="image/*"
+          className="hidden"
+          onChange={fileUploadHandler}
+        />
       </label>
-      <input
-        type="file"
-        id="select-image"
-        accept="image/*"
-        className="hidden"
-        onChange={fileUploadHandler}
-      />
-      {image && <img src={imageURL} alt={image.name} />}
+      {image && (
+        <img
+          src={imageURL}
+          alt={image.name}
+          className="h-full w-full overflow-hidden object-contain bg-gray-50 border-dashed border-2 border-gray-300"
+        />
+      )}
     </div>
   );
 };
