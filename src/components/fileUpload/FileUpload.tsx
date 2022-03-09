e714bfff56941baccd89a3fbcbd821069abc2bb5
Michael Desanker <mdesanker@gmail.com>
2022-03-08 22:31:26 -0500

    Add useEffect hook to generate url strings from input

--- src/components/fileUpload/FileUpload.tsx
+++ src/components/fileUpload/FileUpload.tsx
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const FileUpload = () => {
-  const [images, setImages] = useState<FileList | null>(null);
+  const [images, setImages] = useState<FileList | null>();
+  const [imageURLs, setImageURLs] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (images) {
+      if (images.length === 0) return;
+      const newImageUrls = [];
+      for (let i = 0; i < images.length; i++) {
+        newImageUrls.push(URL.createObjectURL(images[i]));
+        console.log(URL.createObjectURL(images[i]));
+      }
+      setImageURLs(newImageUrls);
+    }
+  }, [images]);
 
   const fileUploadHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setImages(e.target.files);
-    console.log(e.target.files);
   };
 
   return (
     <div className="bg-white rounded-lg shadow-lg p-6">
       <input
         type="file"
-        multiple
+        // multiple
         accept="image/*"
         onChange={fileUploadHandler}
       />
