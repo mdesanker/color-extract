8877daf35b566d0211d9516cd9cd9279e5167009
Michael Desanker <mdesanker@gmail.com>
2022-03-10 10:51:43 -0500

    Load colors from state in color panel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { usePalette } from "react-palette";
+import React from "react";
 import "./App.css";
 import FileContainer from "./components/fileContainer/FileContainer";
 import Header from "./components/header/Header";
 import ColorPanel from "./components/palette/ColorPanel";
 
 const App = () => {
-  const [image, setImage] = useState<any>(null);
-  const [imageURL, setImageURL] = useState("");
-
-  useEffect(() => {
-    if (image) {
-      setImageURL(URL.createObjectURL(image));
-    }
-  }, [image]);
-
-  const { data } = usePalette(imageURL);
-
-  const colorArray = [];
-  for (let key of Object.keys(data)) {
-    colorArray.push({
-      color: data[key],
-    });
-  }
-
   return (
     <>
       <Header />
       <FileContainer />
-      {/* <ColorPanel colors={colorArray} /> */}
+      <ColorPanel />
     </>
   );
 };
--- src/components/palette/ColorPanel.tsx
+++ src/components/palette/ColorPanel.tsx
@@ -1,11 +1,26 @@
+import { usePalette } from "react-palette";
+import { useAppSelector } from "../../store/hooks";
+import { RootState } from "../../store/store";
 import ColorSwatch from "./ColorSwatch";
+import { v4 as uuidv4 } from "uuid";
 
-const ColorPanel = ({ colors }: any) => {
+const ColorPanel = () => {
+  const { imageURL } = useAppSelector((state: RootState) => state.image);
+
+  const { data } = usePalette(imageURL);
+
+  const colors = [];
+  for (let key of Object.keys(data)) {
+    colors.push({
+      color: data[key],
+    });
+  }
+
   return (
     <div className="w-9/10 max-w-[500px] h-28 border rounded-3xl shadow-lg flex overflow-hidden">
       {colors &&
         colors.map((color: any) => {
-          return <ColorSwatch color={color.color} />;
+          return <ColorSwatch key={uuidv4()} color={color.color} />;
         })}
     </div>
   );
