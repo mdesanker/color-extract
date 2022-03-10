b40490b672030516d5121ed88af430a8cab02abc
Michael Desanker <mdesanker@gmail.com>
2022-03-09 21:04:01 -0500

    Style palette as continuous bar

--- src/components/palette/ColorPanel.tsx
+++ src/components/palette/ColorPanel.tsx
@@ -2,7 +2,7 @@ import ColorSwatch from "./ColorSwatch";
 
 const ColorPanel = ({ colors }: any) => {
   return (
-    <div className="flex items-center gap-2">
+    <div className="w-9/10 max-w-[500px] h-28 border rounded-3xl shadow-lg flex overflow-hidden">
       {colors &&
         colors.map((color: any) => {
           return <ColorSwatch color={color.color} />;
--- src/components/palette/ColorSwatch.tsx
+++ src/components/palette/ColorSwatch.tsx
@@ -4,13 +4,10 @@ interface Props {
 
 const ColorSwatch = ({ color }: Props) => {
   return (
-    <div className="flex flex-col gap-1 p-2">
-      <div
-        className={`h-20 w-20 border border-gray-400 shadow-md`}
-        style={{ backgroundColor: color }}
-      ></div>
-      <p className="text-center">{color}</p>
-    </div>
+    <>
+      <div className="h-full w-1/6" style={{ backgroundColor: color }}></div>
+      <p className="text-center hidden">{color}</p>
+    </>
   );
 };
 
