bb31ddf706ea2554e2daad1c53c54c37f7bc0601
Michael Desanker <mdesanker@gmail.com>
2022-03-10 06:32:25 -0500

    Show hex code on hover

--- src/components/palette/ColorSwatch.tsx
+++ src/components/palette/ColorSwatch.tsx
@@ -8,11 +8,11 @@ const ColorSwatch = ({ color }: Props) => {
   return (
     <>
       <div
-        className="h-full w-1/6 cursor-pointer"
+        className="h-full w-1/6 cursor-pointer group"
         style={{ backgroundColor: color }}
       >
         <p
-          className={`w-full h-full flex justify-center items-center text-center uppercase ${
+          className={`w-full h-full flex justify-center items-center text-center uppercase font-medium opacity-0 group-hover:opacity-100 ${
             brightness(color.slice(1)) > 125 ? "text-black" : "text-white"
           }`}
         >
