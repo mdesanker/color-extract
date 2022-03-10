321975d9e3443400616fa62a63283e8b9f56ca32
Michael Desanker <mdesanker@gmail.com>
2022-03-10 16:48:37 -0500

    Style dropzone

--- src/components/dropzone/Dropzone.tsx
+++ src/components/dropzone/Dropzone.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Dropzone from "react-dropzone";
+import { BsCardImage } from "react-icons/bs";
 
 const DropZone = () => {
   const dropHandler = (acceptedFiles: any) => {
@@ -8,10 +9,18 @@ const DropZone = () => {
 
   return (
     <Dropzone onDrop={dropHandler} accept="image/*" multiple={false}>
-      {({ getRootProps, getInputProps }) => (
-        <div {...getRootProps()}>
+      {({ getRootProps, getInputProps, isDragActive }) => (
+        <div
+          {...getRootProps()}
+          className="w-9/10 max-w-[600px] h-[300px] rounded-lg bg-gray-100 p-6 border-dashed border-2 border-gray-300 flex flex-col justify-center items-center cursor-pointer"
+        >
           <input {...getInputProps()} />
-          <p>Drop image here or click to browse</p>
+          <BsCardImage className="h-16 w-16 text-gray-400" />
+          <p className="text-center text-gray-500">
+            {isDragActive
+              ? "Drop file here"
+              : "Drop image here or click to browse"}
+          </p>
         </div>
       )}
     </Dropzone>
