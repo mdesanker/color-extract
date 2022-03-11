0fb265fbb50d51776aaab1d0990e9329fc18618d
Michael Desanker <mdesanker@gmail.com>
2022-03-11 06:39:01 -0500

    Change alert msg color

--- src/components/alert/elements/AlertMsg.tsx
+++ src/components/alert/elements/AlertMsg.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 const AlertMsg = ({ msg }: Props) => {
   return (
-    <div className="flex items-center justify-center gap-4 px-6 py-3 rounded-full bg-cyan-900 shadow-md mb-2 text-sm">
+    <div className="flex items-center justify-center gap-4 px-6 py-3 rounded-full bg-orange-700 shadow-md mb-2 text-sm">
       <div className="rounded-full p-1 bg-white">
-        <FaCheck className="text-cyan-900" />
+        <FaCheck className="text-orange-700" />
       </div>
       <p className="text-white">{msg}</p>
     </div>
