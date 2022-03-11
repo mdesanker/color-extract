388fca7d12e2711b57a35659b1310d740d91bb71
Michael Desanker <mdesanker@gmail.com>
2022-03-11 13:54:11 -0500

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Alerts from "./components/alert/Alerts";
 import Dropzone from "./components/dropzone/Dropzone";
 import Header from "./components/header/Header";
