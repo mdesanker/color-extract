6c27f329eb734d13b1f760c423f3f1bf644d507f
Michael Desanker <mdesanker@gmail.com>
2022-03-11 09:53:18 -0500

    Update header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,6 +4,9 @@ const Header = () => {
       <h1 className="text-4xl text-black font-medium text-center mb-2">
         Color Extractor
       </h1>
+      <p className="text-center text-gray-600">
+        Upload an image to extract prominent colors in hex codes
+      </p>
     </div>
   );
 };
