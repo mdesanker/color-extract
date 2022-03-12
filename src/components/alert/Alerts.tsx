617a00486ffbcf44257e122e26d763e94a43e38f
Michael Desanker <mdesanker@gmail.com>
2022-03-12 10:31:29 -0500

    Minor changes to alert styling

--- src/components/alert/Alerts.tsx
+++ src/components/alert/Alerts.tsx
@@ -6,7 +6,7 @@ const Alerts = () => {
   const alerts = useAppSelector((state: RootState) => state.alert);
 
   return (
-    <div className="fixed bottom-0 w-5/6 max-w-[350px] flex flex-col items-center">
+    <div className="fixed bottom-3 w-5/6 max-w-[350px] flex flex-col items-center">
       {alerts &&
         alerts.map((alert) => {
           return <AlertMsg key={alert.id} msg={alert.msg} />;
--- src/components/alert/elements/AlertMsg.tsx
+++ src/components/alert/elements/AlertMsg.tsx
@@ -6,11 +6,11 @@ interface Props {
 
 const AlertMsg = ({ msg }: Props) => {
   return (
-    <div className="flex items-center justify-center gap-4 px-6 py-3 rounded-full bg-orange-700 shadow-md mb-2 text-sm">
+    <div className="flex items-center justify-center gap-4 px-6 py-3 rounded-full bg-gray-700 opacity-90 shadow-md mb-2 text-sm">
       <div className="rounded-full p-1 bg-white">
-        <FaCheck className="text-orange-700" />
+        <FaCheck className="text-gray-700" />
       </div>
-      <p className="text-white">{msg}</p>
+      <p className="text-white font-medium">{msg}</p>
     </div>
   );
 };
